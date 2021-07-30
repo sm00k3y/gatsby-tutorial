0ed558c4c273bb8ca23c554553858916347d4170
sm00k3y <kubasmolka1999@gmail.com>
2021-07-30 10:39:22 +0200

    Adding image plugin and first image - tutorial p. 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from "react"
 import Layout from '../components/layout'
+import { StaticImage } from "gatsby-plugin-image"
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby tutorial.</p>
+      <StaticImage 
+        alt="Mr Robot poster"
+        src="../images/robot.jpg"
+      />
     </Layout>
   )
 }
