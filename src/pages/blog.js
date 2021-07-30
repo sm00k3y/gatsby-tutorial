6fe014cb60a3d7ea3414c9cb74999f1fb8c37330
sm00k3y <kubasmolka1999@gmail.com>
2021-07-30 13:52:50 +0200

    Adding blog posts - tutorial p. 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 
 
 const BlogPage = ({ data }) => {
@@ -11,10 +12,14 @@ const BlogPage = ({ data }) => {
     <Layout pageTitle="Blog">
       <ul>
         {
-          data.allFile.nodes.map(node => (
-            <li key={node.name}>
-              {node.name}
-            </li>
+          data.allMdx.nodes.map((node) => (
+            <article key={node.id}>
+              <h2>{node.frontmatter.title}</h2>
+              <p>Posted: {node.frontmatter.date}</p>
+              <MDXRenderer>
+                {node.body}
+              </MDXRenderer>
+            </article>
           ))
         }
       </ul>
@@ -25,9 +30,14 @@ const BlogPage = ({ data }) => {
 
 export const query = graphql`
   query {
-    allFile {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "DD-MM-YYYY")
+          title
+        }
+        id
+        body
       }
     }
   }
